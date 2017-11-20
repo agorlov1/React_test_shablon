d1bc7c80212f2dbf9591d3757f46cbc496d2f9c0
Agorlov1 <agorlov2@gmail.com>
2017-11-20 18:48:29 +0700

    Chage

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 2. Run `npm i eslint webpack-dev-server webpack eslint-plugin-react -g`
 3. Run `npm run dev`
 4. Open `localhost:3000`
+
+5.Запуск
+ 
+F:\Web_disiner\Lessons\React\react\ReactJS, Redux и ES2015 с Нуля до Гуру\ReactJS, Redux и ES2015 с Нуля до Гуру
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hi1 sdfsdf');
+import React from 'react';
+import ReactDOM from 'react-dom';
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>App worksfdxfgfd</h1>
+                <h3>sdfsfsddsfsfd</h3>
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    <App/>,
+    document.getElementById('App')
+);
+
